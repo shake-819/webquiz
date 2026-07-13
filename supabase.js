682b5bc0c83cedd7f619dca2d9369a88e5c78a83
shake-819 <132436249@gse.okayama-c.ed.jp>
2026-07-13 16:27:21 +0900

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+"https://rzqhvomycxqqzudhpzen.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_8p4zhhVZ1qavPtPSi595WQ_1ZtblEBi";
+
+const supabaseClient =
+supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
