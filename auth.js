35634298a349f85ea382609d780203962952fca0
shake-819 <132436249@gse.okayama-c.ed.jp>
2026-07-14 15:17:34 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,33 +6,42 @@ loginBtn?.addEventListener("click", login);
 
 async function register() {
 
-    const email =
-        document.getElementById("email").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
 
-    const password =
-        document.getElementById("password").value;
-
-    const { error } =
+    const { data, error } =
         await supabaseClient.auth.signUp({
-
             email,
             password
-
         });
 
     if (error) {
-
         alert(error.message);
-
-    } else {
-
-        alert("登録しました");
-
-        location.href = "login.html";
-
+        return;
     }
-}
 
+    // 登録したユーザー
+    const user = data.user;
+
+    // usersテーブルに初期データを作成
+    const { error: insertError } =
+        await supabaseClient
+            .from("users")
+            .insert({
+                id: user.id,
+                score: 0,
+                correct: 0,
+                wrong: 0
+            });
+
+    if (insertError) {
+        console.error(insertError);
+    }
+
+    alert("登録しました");
+
+    location.href = "login.html";
+}
 async function login() {
 
     const email =
